7d7228c24322655f4e039246ae1d43a18174647f
springrain <chunanyong@163.com>
2020-03-09 23:01:24 +0800

    增加ApiResult

--- ApiResult.go
+++ ApiResult.go
@@ -24,6 +24,61 @@ func newAPIResult(jsonstr string) (*APIResult, error) {
 }
 
 //IsSuccess 是否成功
-func (apiResult *APIResult) IsSuccess() {
+func (apiResult *APIResult) IsSuccess() bool {
+	errorCode := apiResult.getErrorCode()
+	// errorCode 为 0 时也可以表示为成功,详见: https://developers.weixin.qq.com/doc/offiaccount/Getting_Started/Global_Return_Code.html
+	return errorCode == 0
+}
 
+func (apiResult *APIResult) getErrorCode() int {
+	return apiResult.getInt("errcode")
+}
+
+func (apiResult *APIResult) getErrorMsg() string {
+	errorCode := apiResult.getInt("errcode")
+	errMsg, ok := errCodeToErrMsgMap[errorCode]
+	if ok {
+		return errMsg
+	}
+	return apiResult.get("errmsg")
+}
+
+func (apiResult *APIResult) getOpenID() string {
+	return apiResult.get("openId")
+}
+func (apiResult *APIResult) getUnionID() string {
+	return apiResult.get("unionid")
+}
+func (apiResult *APIResult) getSessionKey() string {
+	return apiResult.get("session_key")
+}
+func (apiResult *APIResult) getScope() string {
+	return apiResult.get("scope")
+}
+func (apiResult *APIResult) getAccessToken() string {
+	return apiResult.get("access_token")
+}
+func (apiResult *APIResult) isAccessTokenInvalid() bool {
+	errorCode := apiResult.getInt("errcode")
+	return errorCode == -2 || errorCode == 40001 || errorCode == 42001 || errorCode == 42002 || errorCode == 40014
+}
+func (apiResult *APIResult) getExpiresIn() int {
+	return apiResult.getInt("expires_in")
+}
+
+func (apiResult *APIResult) get(name string) string {
+	value := apiResult.Attrs[name]
+	stringValue, intOk := value.(string)
+	if !intOk {
+		return ""
+	}
+	return stringValue
+}
+func (apiResult *APIResult) getInt(name string) int {
+	value := apiResult.Attrs[name]
+	intValue, intOk := value.(int)
+	if !intOk {
+		return -2
+	}
+	return intValue
 }
