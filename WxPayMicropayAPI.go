d0b3a0d130d20b04827406fa625c8f39cb8bb3d4
springrain <chunanyong@163.com>
2020-03-19 22:50:50 +0800

    修改依赖

--- WxPayMicropayAPI.go
+++ WxPayMicropayAPI.go
@@ -2,6 +2,14 @@ package gowe
 
 import "encoding/xml"
 
+// 场景信息模型
+type SceneInfoModel struct {
+	ID       string `json:"id"`        // 门店唯一标识
+	Name     string `json:"name"`      // 门店名称
+	AreaCode string `json:"area_code"` // 门店所在地行政区划码，详细见《最新县及县以上行政区划代码》
+	Address  string `json:"address"`   // 门店详细地址
+}
+
 // 提交付款码支付的参数
 type MicropayBody struct {
 	SignType       string `json:"sign_type,omitempty"`   // 签名类型，目前支持HMAC-SHA256和MD5，默认为MD5
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module gitee.com/chunanyong/gowe
 
 go 1.14
+
+require (
+
+github.com/beevik/etree v1.1.0
+
+)
